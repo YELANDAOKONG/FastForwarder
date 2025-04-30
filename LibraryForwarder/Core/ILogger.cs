09b531aac1fd775ff999c4149ae3db1f3a86c569
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-05-01 01:05:53 +0800

    Add ILogger

---
+++ LibraryForwarder/Core/ILogger.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryForwarder.Core;
+
+public interface ILogger
+{
+    public void Log(string level, params string[] messages);
+    
+    public void Off(params string[] messages);
+    public void Trace(params string[] messages);
+    public void Debug(params string[] messages);
+    public void Info(params string[] messages);
+    public void Warn(params string[] messages);
+    public void Error(params string[] messages);
+    public void Fatal(params string[] messages);
+    public void All(params string[] messages);
+}
