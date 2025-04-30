e80f60ce5478577371e3b6d94f96eebced780676
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-05-01 01:29:42 +0800

    Init CenterServer

---
+++ LibraryForwarder/Core/Center/CenterServer.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using LibraryForwarder.Core.Center.Models;
+
+namespace LibraryForwarder.Core.Center;
+
+public class CenterServer
+{
+    public IPAddress ServerAddress;
+    public int ServerPort;
+    
+    public Dictionary<byte[], NodeGroup> Groups = new();
+    public ReaderWriterLockSlim GroupsLock = new();
+
+    private ILogger _logger;
+    private Socket _socket;
+    private bool _isWorking = false;
+    
+    public CenterServer(IPAddress address, int port, ILogger? logger = null)
+    {
+        ServerAddress = address;
+        ServerPort = port;
+        
+        
+        _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        _socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+        _socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+        _socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
+        _socket.Bind(new IPEndPoint(ServerAddress, ServerPort));
+    }
+
+
+
+    public async void MainThread()
+    {
+        while (_isWorking)
+        {
+            try
+            {
+                var clientSocket = await _socket.AcceptAsync();
+                Console.WriteLine($"[INFO] ({clientSocket.AddressFamily}): New connection from {clientSocket.RemoteEndPoint}");
+                await Task.Run(() => WorkingThread(clientSocket));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    public async void WorkingThread(Socket clientSocket)
+    {
+        
+    }
+
+    public async void WorkingThreadNodeToNode()
+    {
+        
+    }
+}
---
+++ LibraryForwarder/Core/Center/Models/NodeGroup.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Sockets;
+
+namespace LibraryForwarder.Core.Center.Models;
+
+public record NodeGroup
+{
+    public byte[] SessionId = [];
+    public required Socket Provider;
+    public List<Socket> Visitors = new();
+}
